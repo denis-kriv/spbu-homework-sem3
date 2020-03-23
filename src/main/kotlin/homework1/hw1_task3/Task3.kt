2ba1218cedb1c70a44e582f70b6c3f177d29f42b
Denis <den89181827071@mail.ru>
2020-03-23 19:51:58 +0300

    add solution and tests for hw1_task3

---
+++ src/main/kotlin/homework1/hw1_task3/Task3.kt
@@ -0,0 +1,32 @@
+package homework1.hw1_task3
+import java.util.*
+
+fun countOfSubstrings(firstString: String, secondString: String): Int {
+    if (firstString.length < secondString.length) {
+        return 0
+    }
+    if (firstString.length == secondString.length) {
+        if (firstString == secondString) {
+            return 1
+        }
+        return 0
+    }
+    if (firstString.length > secondString.length) {
+        var result = 0
+        for (i in 0..firstString.length - secondString.length) {
+            if (firstString.substring(i, i + secondString.length) == secondString)
+                result++
+        }
+        return result
+    }
+    return 0
+}
+
+fun main() {
+    val scan = Scanner(System.`in`)
+    println("Введите первую строку")
+    val firstInputString = scan.nextLine()
+    println("Введите вторую строку")
+    val secondInputString = scan.nextLine()
+    println("Количество вхождений второй строки в первую: ${countOfSubstrings(firstInputString, secondInputString)}")
+}
---
+++ src/test/kotlin/homework1Tests/TestsForTask3.kt
@@ -0,0 +1,65 @@
+package homework1Tests
+
+import homework1.hw1_task3.countOfSubstrings
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class TestsForTask3 {
+
+    @Test
+    fun testForOneChar() {
+        val firstTestString = "aaaaaa"
+        val secondTestString = "a"
+        val result = 6
+        assertEquals(result, countOfSubstrings(firstTestString, secondTestString))
+    }
+
+    @Test
+    fun testForSpaces() {
+        val firstTestString = "  "
+        val secondTestString = " "
+        val result = 2
+        assertEquals(result, countOfSubstrings(firstTestString, secondTestString))
+    }
+
+    @Test
+    fun testForStringWithTwoEntriesInARow() {
+        val firstTestString = "abcabc"
+        val secondTestString = "abc"
+        val result = 2
+        assertEquals(result, countOfSubstrings(firstTestString, secondTestString))
+    }
+
+    @Test
+    fun testForStringWithEntriesNotInARow() {
+        val firstTestString = "aabaabab"
+        val secondTestString = "ab"
+        val result = 3
+        assertEquals(result, countOfSubstrings(firstTestString, secondTestString))
+    }
+
+    @Test
+    fun testForStringWithSameChars() {
+        val firstTestString = "aadddddaddd"
+        val secondTestString = "ddd"
+        val result = 4
+        assertEquals(result, countOfSubstrings(firstTestString, secondTestString))
+    }
+
+    @Test
+    fun testWhenFirstStringIsSmallerThanSecond() {
+        val firstTestString = "aa"
+        val secondTestString = "abc"
+        val result = 0
+        assertEquals(result, countOfSubstrings(firstTestString, secondTestString))
+    }
+
+    @Test
+    fun testWhenStringsAreEquals() {
+        val firstTestString = "acc"
+        val secondTestString = "acc"
+        val result = 1
+        assertEquals(result, countOfSubstrings(firstTestString, secondTestString))
+    }
+}
