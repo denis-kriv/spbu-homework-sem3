f69efb55b8eb63ce9d0208952c2ed529c17f2eee
Denis <den89181827071@mail.ru>
2020-05-30 02:35:36 +0300

    fix

--- src/main/kotlin/tests/test2/task1/bubbleSort.kt
+++ src/main/kotlin/tests/test2/task1/bubbleSort.kt
@@ -5,8 +5,8 @@ import java.lang.NullPointerException
 fun <T> bubbleSort(array: Array<T>?, comparator: Comparator<T>): Array<T> {
     if (array.isNullOrEmpty()) throw NullPointerException("array is null")
     for (i in array.indices) {
-        for (j in array.size - 1 downTo i) {
-            if (comparator.compare(array[i], array[j]) == 1) {
+        for (j in array.size - 1 downTo i + 1) {
+            if (comparator.compare(array[j - 1], array[j]) == 1) {
                 val flag = array[j - 1]
                 array[j - 1] = array[j]
                 array[j] = flag
