6586faf2e54a03e07c2f7a773c506eb5eff1e645
Denis <den89181827071@mail.ru>
2020-05-29 16:55:09 +0300

    some fixes for ci

--- src/main/kotlin/tests/test2/task1/bubbleSort.kt
+++ src/main/kotlin/tests/test2/task1/bubbleSort.kt
@@ -7,9 +7,9 @@ fun <T> bubbleSort(array: Array<T>?, comparator: Comparator<T>): Array<T> {
     for (i in array.indices) {
         for (j in array.size - 1 downTo i) {
             if (comparator.compare(array[i], array[j]) == 1) {
-                val flag = array[j - 1];
-                array[j - 1] = array[j];
-                array[j] = flag;
+                val flag = array[j - 1]
+                array[j - 1] = array[j]
+                array[j] = flag
             }
         }
     }
