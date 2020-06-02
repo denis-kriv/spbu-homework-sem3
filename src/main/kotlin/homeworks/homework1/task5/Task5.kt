f0dc4ede4851a1c8c65d3d981fb25393f5f8a6eb
Denis <den89181827071@mail.ru>
2020-06-02 13:46:41 +0300

    change function for search not empty string

--- src/main/kotlin/homeworks/homework1/task5/Task5.kt
+++ src/main/kotlin/homeworks/homework1/task5/Task5.kt
@@ -1,22 +1,15 @@
 package homeworks.homework1.task5
 
-import java.util.Scanner
 import java.io.File
 
 fun countOfStrings(filePath: String): Int {
     val listOfFileStrings = File(filePath).useLines { it.toList() }
-    var result = 0
-    listOfFileStrings.forEach {
-        if (!it.isNullOrBlank()) {
-            result++
-        }
-    }
-    return result
+    return listOfFileStrings.count { it.isNotBlank() }
 }
 
 fun main() {
-    val scan = Scanner(System.`in`)
     println("Введите путь к файлу")
-    val filePath = scan.nextLine()
-    println("Количество непустых строк: ${countOfStrings(filePath)}")
+    val filePath = readLine()
+    if (filePath.isNullOrEmpty()) println("Строка ввода пуста")
+    else println("Количество непустых строк: ${countOfStrings(filePath)}")
 }
