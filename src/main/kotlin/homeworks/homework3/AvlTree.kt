f08ce895cefa56b6604937e60c01e190e80059c8
DenisKrivoruchko11 <den89181827071@mail.ru>
2020-07-29 16:05:09 +0100

    fix ci

--- src/main/kotlin/homeworks/homework3/AvlTree.kt
+++ src/main/kotlin/homeworks/homework3/AvlTree.kt
@@ -2,6 +2,62 @@ package homeworks.homework3
 
 import java.lang.Exception
 
+private fun<K: Comparable<K>, T> getValueByKey(key: K, item: AvlTreeItem<K, T>?): T? {
+    if (item == null) return null
+
+    return when {
+        key > item.key -> getValueByKey(key, item.rightChild)
+        key < item.key -> getValueByKey(key, item.leftChild)
+        else -> item.value
+    }
+}
+
+private fun<K: Comparable<K>, T> insert(treeHead: AvlTreeItem<K, T>?, insertingItem: Pair<K, T>): AvlTreeItem<K, T> {
+    if (treeHead == null) return AvlTreeItem(insertingItem.first, insertingItem.second)
+
+    if (insertingItem.first < treeHead.key) {
+        treeHead.leftChild = insert(treeHead.leftChild, insertingItem)
+    } else {
+        treeHead.rightChild = insert(treeHead.rightChild, insertingItem)
+    }
+
+    return treeHead.balance()
+}
+
+private fun<K: Comparable<K>, T> findMinKey(item: AvlTreeItem<K, T>): AvlTreeItem<K, T> {
+    return if (item.leftChild == null) item else findMinKey(item.leftChild!!)
+}
+
+private fun<K: Comparable<K>, T> removeMinKey(item: AvlTreeItem<K, T>): AvlTreeItem<K, T>? {
+    if (item.leftChild == null) return item.rightChild
+
+    item.leftChild = removeMinKey(item.leftChild!!)
+
+    return item.balance()
+}
+
+private fun<K: Comparable<K>, T> remove(item: AvlTreeItem<K, T>?, key: K): AvlTreeItem<K, T>? {
+    if (item == null) throw NoSuchElementException("Element with this key does not exist.")
+
+    return when {
+        key < item.key -> {
+            item.leftChild = remove(item.leftChild, key)
+            item.balance()
+        }
+        key > item.key -> {
+            item.rightChild = remove(item.rightChild, key)
+            item.balance()
+        }
+        else -> {
+            if (item.rightChild == null) return item.leftChild
+            val minKey = findMinKey(item.rightChild!!)
+            minKey.rightChild = removeMinKey(item.rightChild!!)
+            minKey.leftChild = item.leftChild
+            minKey.balance()
+        }
+    }
+}
+
 class AvlTree<K : Comparable<K>, T>(key: K, value: T) : Map<K, T> {
 
     override var entries: Set<Map.Entry<K, T>> = mapOf(key to value).entries
@@ -9,62 +65,6 @@ class AvlTree<K : Comparable<K>, T>(key: K, value: T) : Map<K, T> {
     override var size: Int = 1
     override var values: Collection<T> = listOf(value)
     private var head: AvlTreeItem<K, T>? = AvlTreeItem(key, value)
-
-    private fun getValueByKey(key: K, item: AvlTreeItem<K, T>?): T? {
-        if (item == null) return null
-
-        return when {
-            key > item.key -> getValueByKey(key, item.rightChild)
-            key < item.key -> getValueByKey(key, item.leftChild)
-            else -> item.value
-        }
-    }
-
-    private fun insert(treeHead: AvlTreeItem<K, T>?, insertingItem: Pair<K, T>): AvlTreeItem<K, T> {
-        if (treeHead == null) return AvlTreeItem(insertingItem.first, insertingItem.second)
-
-        if (insertingItem.first < treeHead.key) {
-            treeHead.leftChild = insert(treeHead.leftChild, insertingItem)
-        } else {
-            treeHead.rightChild = insert(treeHead.rightChild, insertingItem)
-        }
-
-        return treeHead.balance()
-    }
-
-    private fun findMinKey(item: AvlTreeItem<K, T>): AvlTreeItem<K, T> {
-        return if (item.leftChild == null) item else findMinKey(item.leftChild!!)
-    }
-
-    private fun removeMinKey(item: AvlTreeItem<K, T>): AvlTreeItem<K, T>? {
-        if (item.leftChild == null) return item.rightChild
-
-        item.leftChild = removeMinKey(item.leftChild!!)
-
-        return item.balance()
-    }
-
-    private fun remove(item: AvlTreeItem<K, T>?, key: K): AvlTreeItem<K, T>? {
-        if (item == null) throw NoSuchElementException("Element with this key does not exist.")
-
-        return when {
-            key < item.key -> {
-                item.leftChild = remove(item.leftChild, key)
-                item.balance()
-            }
-            key > item.key -> {
-                item.rightChild = remove(item.rightChild, key)
-                item.balance()
-            }
-            else -> {
-                if (item.rightChild == null) return item.leftChild
-                val minKey = findMinKey(item.rightChild!!)
-                minKey.rightChild = removeMinKey(item.rightChild!!)
-                minKey.leftChild = item.leftChild
-                minKey.balance()
-            }
-        }
-    }
 
     override fun containsKey(key: K): Boolean {
         return this.keys.contains(key)
