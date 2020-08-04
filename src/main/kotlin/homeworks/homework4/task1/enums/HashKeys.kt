79693db194464cf62bf2882b12075528f2c98435
DenisKrivoruchko11 <den89181827071@mail.ru>
2020-08-04 21:29:23 +0100

    try to fix ci

--- src/main/kotlin/homeworks/homework4/task1/enums/HashKeys.kt
+++ src/main/kotlin/homeworks/homework4/task1/enums/HashKeys.kt
@@ -1,4 +1,17 @@
 package homeworks.homework4.task1.enums
 
-enum class HashKeys(val key: Int) { Hash3(3), Hash5(5), Hash7(7), Hash11(11) }
+enum class HashKeys(val key: Int) {
+    Hash3(Keys.keyForHashKeys3),
+    Hash5(Keys.keyForHashKeys5),
+    Hash7(Keys.keyForHashKeys7),
+    Hash11(Keys.keyForHashKeys11)
+}
 
+private class Keys {
+    companion object {
+        const val keyForHashKeys3: Int = 3
+        const val keyForHashKeys5: Int = 5
+        const val keyForHashKeys7: Int = 7
+        const val keyForHashKeys11: Int = 11
+    }
+}
