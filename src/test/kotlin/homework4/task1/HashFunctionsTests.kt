3884f2763e2ab9fb8344107675467fd74657d05c
DenisKrivoruchko11 <den89181827071@mail.ru>
2020-08-04 12:56:59 +0100

    add a template for HashFunctionsTests

--- src/test/kotlin/homework4/task1/HashFunctionsTests.kt
+++ src/test/kotlin/homework4/task1/HashFunctionsTests.kt
@@ -1,4 +1,66 @@
 package homework4.task1
 
+import org.junit.jupiter.api.Test
+
 class HashFunctionsTests {
+
+    @Test
+    fun getHashWithKey3ShouldReturnsDifferentIndexesForTwoRandomStrings() {
+
+    }
+
+    @Test
+    fun getHashWithKey3ShouldReturnsDifferentIndexesForOneString() {
+
+    }
+
+    @Test
+    fun getHashWithKey3ShouldReturnsCreateCollisionForTwoSpecialStrings() {
+
+    }
+
+    @Test
+    fun getHashWithKey5ShouldReturnsDifferentIndexesForTwoRandomStrings() {
+
+    }
+
+    @Test
+    fun getHashWithKey5ShouldReturnsDifferentIndexesForOneString() {
+
+    }
+
+    @Test
+    fun getHashWithKey5ShouldReturnsCreateCollisionForTwoSpecialStrings() {
+
+    }
+
+    @Test
+    fun getHashWithKey7ShouldReturnsDifferentIndexesForTwoRandomStrings() {
+
+    }
+
+    @Test
+    fun getHashWithKey7ShouldReturnsDifferentIndexesForOneString() {
+
+    }
+
+    @Test
+    fun getHashWithKey7ShouldReturnsCreateCollisionForTwoSpecialStrings() {
+
+    }
+
+    @Test
+    fun getHashWithKey11ShouldReturnsDifferentIndexesForTwoRandomStrings() {
+
+    }
+
+    @Test
+    fun getHashWithKey11ShouldReturnsDifferentIndexesForOneString() {
+
+    }
+
+    @Test
+    fun getHashWithKey11ShouldReturnsCreateCollisionForTwoSpecialStrings() {
+
+    }
 }
