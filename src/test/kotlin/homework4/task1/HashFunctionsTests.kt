fdaf93ffc318bd75c35c39b716ca3bec4e5f5f1d
DenisKrivoruchko11 <den89181827071@mail.ru>
2020-08-04 12:49:36 +0100

    add a template for HashTableTests

---
+++ src/test/kotlin/homework4/task1/HashFunctionsTests.kt
@@ -0,0 +1,4 @@
+package homework4.task1
+
+class HashFunctionsTests {
+}
---
+++ src/test/kotlin/homework4/task1/HashTableTests.kt
@@ -0,0 +1,111 @@
+package homework4.task1
+
+import org.junit.jupiter.api.Test
+
+internal class HashTableTests {
+
+    @Test
+    fun plusShouldThrowsKotlinNullPointerExceptionWhenInputStringIsBlank() {
+
+    }
+
+    @Test
+    fun plusShouldThrowsKotlinNullPointerExceptionWhenInputStringIsNull() {
+
+    }
+
+    @Test
+    fun plusShouldThrowsCloneNotSupportedExceptionWhenInputValueIsAlreadyExist() {
+
+    }
+
+    @Test
+    fun plusShouldAddItemWhenInputStringIsCorrect() {
+
+    }
+
+    @Test
+    fun minusShouldThrowsKotlinNullPointerExceptionWhenInputStringIsBlank() {
+
+    }
+
+    @Test
+    fun minusShouldThrowsKotlinNullPointerExceptionWhenInputStringIsNull() {
+
+    }
+
+    @Test
+    fun minusShouldThrowsNoSuchElementExceptionWhenInputValueIsNotExist() {
+
+    }
+
+    @Test
+    fun minusShouldRemoveItemWhenInputStringIsCorrect() {
+
+    }
+
+    @Test
+    fun getIndexShouldThrowsKotlinNullPointerExceptionWhenInputStringIsBlank() {
+
+    }
+
+    @Test
+    fun getIndexShouldThrowsKotlinNullPointerExceptionWhenInputStringIsNull() {
+
+    }
+
+    @Test
+    fun getIndexShouldThrowsNoSuchElementExceptionWhenInputValueIsNotExist() {
+
+    }
+
+    @Test
+    fun getIndexShouldReturnsItemIndexWhenInputStringIsCorrect() {
+
+    }
+
+    @Test
+    fun getStatisticsShouldReturnsStatistics() {
+
+    }
+
+    @Test
+    fun plusFromFileShouldThrowsKotlinNullPointerExceptionWhenInputStringIsBlank() {
+
+    }
+
+    @Test
+    fun plusFromFileShouldThrowsKotlinNullPointerExceptionWhenInputStringIsNull() {
+
+    }
+
+    @Test
+    fun plusFromFileShouldThrowsNoSuchFIleExceptionWhenFileIsNotExist() {
+
+    }
+
+    @Test
+    fun plusFromFileShouldFillsTableWhenInputPathIsCorrect() {
+
+    }
+
+    @Test
+    fun chooseHashFunctionShouldThrowsKotlinNullPointerExceptionWhenInputStringIsBlank() {
+
+    }
+
+    @Test
+    fun chooseHashFunctionShouldThrowsKotlinNullPointerExceptionWhenInputStringIsNull() {
+
+    }
+
+    @Test
+    fun chooseHashFunctionShouldThrowsIllegalArgumentExceptionWhenInputStringISNotCorrect() {
+
+    }
+
+    @Test
+    fun chooseHashFunctionShouldChangeHashFunctionWhenInputPathIsCorrect() {
+
+    }
+}
